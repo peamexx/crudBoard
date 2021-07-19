8de4fce8d03ccc1c4a1fec28dc5f2b7b7c19e387
= <=>
2021-07-19 10:28:25 +0900

    ADD json 변경, 보일 개수 select box추가

--- db.js
+++ db.js
@@ -1,52 +1,202 @@
 export default [
     {
-        "id": 0,
-        "name": "eifn1",
-        "age": 92
+        "id": "1",
+        "name": "Macauley Robbins",
+        "hobby": "Improv"
     },
     {
-        "id": 1,
-        "name": "냠냠",
-        "age": 43
+        "id": "2",
+        "name": "Amandeep Rush",
+        "hobby": "Soap making"
     },
     {
-        "id": 2,
-        "name": "49gj",
-        "age": 16
+        "id": "3",
+        "name": "Kelly Henson",
+        "hobby": "Candle making"
     },
     {
-        "id": 3,
-        "name": "앋녕",
-        "age": 36
+        "id": "4",
+        "name": "Tommy Woodward",
+        "hobby": "Cross-fit"
     },
     {
-        "id": 4,
-        "name": "039iwn",
-        "age": 23
+        "id": "5",
+        "name": "Rex Couch",
+        "hobby": "Fly fishing"
     },
     {
-        "id": 5,
-        "name": "apple24",
-        "age": 4
+        "id": "6",
+        "name": "Bruno Gonzales",
+        "hobby": "Video game collecting"
     },
     {
-        "id": 6,
-        "name": "카드",
-        "age": 87
+        "id": "7",
+        "name": "Edwin Terrell",
+        "hobby": "Reloading (Shells)"
     },
     {
-        "id": 7,
-        "name": "메밀이",
-        "age": 1
+        "id": "8",
+        "name": "Mayur Kirby",
+        "hobby": "Lapidary"
     },
     {
-        "id": 8,
-        "name": "motjn",
-        "age": 56
+        "id": "9",
+        "name": "Blake Kline",
+        "hobby": "Ziplining"
     },
     {
-        "id": 9,
-        "name": "d9tj2msifh",
-        "age": 2
+        "id": "10",
+        "name": "Nishat Wyatt",
+        "hobby": "Painting"
+    },
+    {
+        "id": "11",
+        "name": "Saniya Hussain",
+        "hobby": "Kayaking"
+    },
+    {
+        "id": "12",
+        "name": "Sacha Robles",
+        "hobby": "Chainsaw carving"
+    },
+    {
+        "id": "13",
+        "name": "Iain Holden",
+        "hobby": "Tarot card reading"
+    },
+    {
+        "id": "14",
+        "name": "Lili Ireland",
+        "hobby": "Cemetery/Tombstone cleaning"
+    },
+    {
+        "id": "15",
+        "name": "Nicole Paterson",
+        "hobby": "Dresshobby"
+    },
+    {
+        "id": "16",
+        "name": "Rhia Rahman",
+        "hobby": "Meat smoking"
+    },
+    {
+        "id": "17",
+        "name": "Fred Thorpe",
+        "hobby": "Falconry"
+    },
+    {
+        "id": "18",
+        "name": "Shiv Ahmed",
+        "hobby": "Vinyl record collecting"
+    },
+    {
+        "id": "19",
+        "name": "Cindy Hays",
+        "hobby": "Coin collecting"
+    },
+    {
+        "id": "20",
+        "name": "Haidar Morse",
+        "hobby": "Rock collecting"
+    },
+    {
+        "id": "21",
+        "name": "Douglas Woods",
+        "hobby": "Backpacking"
+    },
+    {
+        "id": "22",
+        "name": "Velma Crane",
+        "hobby": "Calligraphy"
+    },
+    {
+        "id": "23",
+        "name": "Nelson Braun",
+        "hobby": "Canoeing"
+    },
+    {
+        "id": "24",
+        "name": "Jamie Mclaughlin",
+        "hobby": "Juggling"
+    },
+    {
+        "id": "25",
+        "name": "Ciaron Vickers",
+        "hobby": "Genealogy"
+    },
+    {
+        "id": "26",
+        "name": "Bree Clarkson",
+        "hobby": "Glass blowing"
+    },
+    {
+        "id": "27",
+        "name": "Lucinda Edwards",
+        "hobby": "Impersonations"
+    },
+    {
+        "id": "28",
+        "name": "Evie-May Bourne",
+        "hobby": "Fencing"
+    },
+    {
+        "id": "29",
+        "name": "Rahma Schmitt",
+        "hobby": "Aluminum can collecting"
+    },
+    {
+        "id": "30",
+        "name": "Terence Kerr",
+        "hobby": "Scrapbooking"
+    },
+    {
+        "id": "31",
+        "name": "Hayleigh Goddard",
+        "hobby": "Minature furniture"
+    },
+    {
+        "id": "32",
+        "name": "Mahi Hyde",
+        "hobby": "Archery"
+    },
+    {
+        "id": "33",
+        "name": "Nazia Dodd",
+        "hobby": "Cheese making"
+    },
+    {
+        "id": "34",
+        "name": "Pearl Shannon",
+        "hobby": "Hot air ballooning"
+    },
+    {
+        "id": "35",
+        "name": "Gabriela Sharp",
+        "hobby": "Robotics"
+    },
+    {
+        "id": "36",
+        "name": "Bill Mcculloch",
+        "hobby": "Cartography"
+    },
+    {
+        "id": "37",
+        "name": "Alissia Xiong",
+        "hobby": "Vinthobby clothing collecting"
+    },
+    {
+        "id": "38",
+        "name": "Edmund Christian",
+        "hobby": "Meterology"
+    },
+    {
+        "id": "39",
+        "name": "Jana Kearney",
+        "hobby": "Robotics"
+    },
+    {
+        "id": "40",
+        "name": "Justin Barton",
+        "hobby": "Wine tasting"
     }
-];
+]
--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 import DATA from './db.js';
 
 // tag
-const table = document.querySelector('table');
 const tbody = document.querySelector('tbody');
 const pagination = document.querySelector('.pagination');
 
 // input
 const id = document.querySelector('inpId');
 const username = document.querySelector('inpName');
-const age = document.querySelector('inpAge');
+const hobby = document.querySelector('inpHobby');
+
+// select
+const select = document.querySelector('select');
 
 // button
 const btnRead = document.querySelector('.btnRead');
 
 // data
-const totalCnt = DATA.length; // 총 개수
-const perCnt = 8; // 한페이지 당 보일 개수
+let totalCnt = DATA.length; // 총 개수
+let perCnt = 5; // 한페이지 당 보일 개수
 
 // event
 window.addEventListener('load', createPagi);
 pagination.addEventListener('click', loadD);
 btnRead.addEventListener('click', readD);
+select.addEventListener('change', changePerCnt);
 
 // read
 document.querySelector('.totalCntA').textContent = totalCnt;
 
 // function
+// 게시글 로드(최초+조건)
+function readD(perCnt, startIdx, endIdx) {
+    if(!endIdx) { // 페이지 로딩 시(최초)
+        startIdx = 0;
+        appendData(perCnt, startIdx);
+    } else { // 페이징 눌렀을 때
+        perCnt = endIdx + 1;
+        appendData(perCnt, startIdx, endIdx);
+    }
+
+    document.querySelectorAll('.row')[2].scrollTop = 0
+};
+
 // 페이징 로드(최초)
 function createPagi() {
     let cnt = totalCnt / perCnt;
@@ -43,17 +59,6 @@ function createPagi() {
     readD(perCnt);
 };
 
-// 게시글 로드(최초+조건)
-function readD(perCnt, startIdx, endIdx) {
-    if(!endIdx) { // 페이지 로딩 시(최초)
-        startIdx = 0;
-        resetData(perCnt, startIdx);
-    } else { // 페이징 눌렀을 때
-        perCnt = endIdx + 1;
-        resetData(perCnt, startIdx, endIdx);
-    }
-};
-
 // 페이징 클릭 시 게시물 새로 로드
 function loadD(e) {
     let target = e.target;
@@ -62,28 +67,42 @@ function loadD(e) {
     let startIdx = Number(((perCnt * selected) - perCnt));
     let endIdx = Number(((perCnt * selected) - 1));
 
-    tbody.querySelectorAll('tr').forEach((el) => el.remove()); // 게시물 삭제
+    tbody.querySelectorAll('tr').forEach((el) => el.remove()); // 게시물 전체 삭제
     pagination.querySelectorAll('li').forEach((el) => el.classList.remove('active'));  // 페이징 .active 삭제
-    target.closest('li').classList.add('active'); // 선택된 페이징에 .active 추가
+    
+    if(target.closest('li')) { // 페이징 클릭 시
+        target.closest('li').classList.add('active'); // 선택된 페이징에 .active 추가
+    } else if(target.closest('select')) { // select 클릭 시
+        pagination.querySelectorAll('li').forEach((el) => el.remove()); // 페이징 전체 삭제
+        createPagi();
+    }
 
     readD(perCnt, startIdx, endIdx);
 };
 
-// 게시물 생성(코어)
-function resetData(perCnt, startIdx, endIdx) {
+// select box 클릭 시 게시물 새로 로드
+function changePerCnt(e) {
+    let value = e.target.value;
+    perCnt = value;
+    loadD(e);
+}
+
+// 게시물 append
+function appendData(perCnt, startIdx, endIdx) {
     DATA.forEach((el, index) => {
+        const {id, name, hobby} = el;
         if(startIdx <= index && index < perCnt) {
             let tr = document.createElement('tr');
             let td = document.createElement('td');
-            td.textContent = el.id;
+            td.textContent = id;
             tr.append(td);
     
             let td2 = document.createElement('td');
-            td2.textContent = el.name;
+            td2.textContent = name;
             tr.append(td2);
     
             let td3 = document.createElement('td');
-            td3.textContent = el.age;
+            td3.textContent = hobby;
             tr.append(td3);
     
             tbody.append(tr);
